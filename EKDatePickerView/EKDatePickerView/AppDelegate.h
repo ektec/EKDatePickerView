7e3a383ea3f86feda24bc2c8ebd27db04e6060ea
huangdl3000 <hdlpillar@126.com>
2015-04-15 22:52:56 +0800

    0
    刚创建工程

---
+++ EKDatePickerView/EKDatePickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EKDatePickerView
+//
+//  Created by huangdl on 15/4/15.
+//  Copyright (c) 2015年 黄驿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
